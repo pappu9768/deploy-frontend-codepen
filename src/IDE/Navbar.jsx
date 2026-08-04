50861efa7bb98ca7be8dbfcd79eb011045e92e7c
Pawan Pramod Shah <9930pawanshah@gmail.com>
2026-08-04 17:51:21 +0530

    Update Navbar.jsx

--- src/IDE/Navbar.jsx
+++ src/IDE/Navbar.jsx
@@ -1,1 +1,24 @@
+import React from "react";
+import "./Navbar.css";
 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <span className="logo-icon">{"</>"}</span>
+        <h2>OnlineIDE</h2>
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/mycodes">My Codes</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
